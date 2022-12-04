30f12e7ed3ac17e414a27077249e1028e5ad18da
KieranIsANerd <90479750+KieranIsANerd@users.noreply.github.com>
2022-12-04 16:54:55 -0500

    Added some constructors for HashTable & HashNode, made more empty functions for Hash Table

--- hash.cpp
+++ hash.cpp
@@ -15,23 +15,31 @@ struct HashNode {
             tuple<string, string, int> flightData;
             HashNode* next;
 
+        //Default HashNode Constructor (NULL)
+        HashNode::HashNode(){
+            this->hashKey = " ";
+            get<0>(this->flightData) = " ";
+            get<1>(this->flightData) = " ";
+            get<2>(this->flightData) = -1;
+            this->next = nullptr;
+        }
         //Hash Node Constructor: hashKey, tuple, and next node
-    //    HashNode::HashNode(string dateFlightNumberKey, tuple<string, string, int> newFlightData, HashNode* next){
-    //         this->hashKey = dateFlightNumberKey;
-    //         this->flightData = newFlightData;
-    //         this->next = next;
-    //    }
+        HashNode::HashNode(string dateFlightNumberKey, tuple<string, string, int> newFlightData, HashNode* next){
+            this->hashKey = dateFlightNumberKey;
+            this->flightData = newFlightData;
+            this->next = next;
+       }
 
 //===============Accessors==============//
-       string getHashKey() const{
+       string HashNode::getHashKey() const{
             return hashKey;
        }
     
-       string getOriginAirport() const{
+       string HashNode::getOriginAirport() const{
             return get<0>(flightData);
        }
 
-       string getDestinationAirport() const{
+       string HashNode::getDestinationAirport() const{
             return get<1>(flightData);
        }
        
@@ -73,22 +81,53 @@ struct HashNode {
         string newDateFlightNumber;
  };
 
-//Hash Structure is a vector of head HashNodes.
-class HashMap {
+//Hash Structure is a vector of head HashNodes
+//Separate Chaining Collision resolution
+class HashTable {
     public: 
-        //Vector is a list of the bucket heads
+        //Vector when initialized is 20, meaning there are 20 buckets in the hash map
+        int const initialBucketListSize = 20; 
+        //Vector is a list of the buckets, buckets are heads for linked lists 
         vector<HashNode*> bucketList;
-        double loadFactor;
-        int numberOfHashNodes; 
+        //Load Factor (# of nodes/ # of buckets), if hashmap is 80% full rehash 
+        double const maxLoadFactor = 0.80;
+        double currentLoadFactor;
+        double numberOfHashNodes;
+        
     
-    //Adds a new pointer to a head hashNode
-    void newBucket(string newDateFlightNumber, tuple<string, string, int> newFlightData){
+    //Creates empty vector of size initialBucket
+    HashTable::HashTable(){
+        numberOfHashNodes = 0;
+        currentLoadFactor = 0; 
+    
+        //Initializing bucketList with empty nodes
+        for (int i = 0; i < initialBucketListSize; i++){
+            HashNode* tempHash = new HashNode();
+            bucketList.push_back(tempHash);
+        }
+    };
+
+    
+    void HashTable::newNode(string newDateFlightNumber, tuple<string, string, int> newFlightData){
         //Set Functions
+
+        //Run Hash Function (Returns index for bucketList vector) Index is the hash value
+        //Either new linked list node or linked list head (bucket node/ collision node)
+
+        //Change head/bucket node data in vector[index]
         
 
     };
     
+    //Initializes Bucket
+    void HashTable::newBucket(string newDataFlightNumber, tuple<string, string, int> newFlightData){
 
+    };
+
+    //Initializes regular hash node/collision node
+    void HashTable::newHashNode(string newDataFlightNumber, tuple<string,string,int> newFlightData){
+
+    };
     
 };
 
