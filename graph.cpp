58faaba1c7c1f5ab176305eec0a0d1c6e37e8ec0
Brayden Bevels <brayden.bevels@ufl.edu>
2022-12-03 20:58:38 -0500

    getAvgDelay

--- graph.cpp
+++ graph.cpp
@@ -13,12 +13,16 @@ class graph{
         // key = source
         // tuple: destination, delay, flight num
         unordered_map<string, vector<tuple<string, int, int>>> airportMap;
+        vector<tuple<string, int, int>> tempDataStorage;
 
     public:
+
+        //
+
         graph();
         //tuple being passed: source, destination, delay, flightNum
         void addEdge(tuple<string, string, string, string> passedData){
-            vector<tuple<string, int, int>> tempPassedDataStorage;
+            vector<tuple<string, int, int>> tempDataStorage;
             //get all data from every index of tuple
             string sourceAirport = get<0>(passedData);
             string destinationAirport = get<1>(passedData);
@@ -27,13 +31,33 @@ class graph{
             //find sourceAirport if already exists
             auto iteratorFindSource = airportMap.find(sourceAirport);
             if(iteratorFindSource != airportMap.end()){ //if already exists, add another tuple to the vector with required data
-                tempPassedDataStorage = iteratorFindSource->second;
-                tempPassedDataStorage.push_back(make_tuple(destinationAirport, delayValue, flightNumber));
-                iteratorFindSource->second = tempPassedDataStorage;
+                tempDataStorage = iteratorFindSource->second;
+                tempDataStorage.push_back(make_tuple(destinationAirport, delayValue, flightNumber));
+                iteratorFindSource->second = tempDataStorage;
             }else{ //if doesnt exist, make new map key-value pair
-                tempPassedDataStorage.push_back(make_tuple(destinationAirport, delayValue, flightNumber));
-                airportMap.emplace(sourceAirport, tempPassedDataStorage);
+                tempDataStorage.push_back(make_tuple(destinationAirport, delayValue, flightNumber));
+                airportMap.emplace(sourceAirport, tempDataStorage);
             }
         };
+
+        // string getWorstAirport(string userAirport){
+
+        // };
+
+        double getAverageDelayTime(string userAirport){
+            int tempDelay;
+            auto findAirportItr = airportMap.find(userAirport); //find iterator to given user airport
+            if(findAirportItr != airportMap.end()){ //userAirport found, add up all delay tims in vector
+                tempDataStorage = findAirportItr->second;
+                for(int i = 0; i < tempDataStorage.size(); i++){
+                    tempDelay += get<2>(tempDataStorage.at(i));
+                }
+            }else{
+                return INT32_MAX;
+            }
+            return tempDelay / tempDataStorage.size();
+        };
+
+
 
 };
