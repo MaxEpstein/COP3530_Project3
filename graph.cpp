f8848736484d7910392016f4081ca602ec6346f1
Brayden Bevels <brayden.bevels@ufl.edu>
2022-12-03 20:17:36 -0500

    Graph update 1

--- graph.cpp
+++ graph.cpp
@@ -1,10 +1,21 @@
+#include <unordered_map>
 using namespace std;
 
 
 
-
+//origin, destination, flight num, arrival delay
 
 class graph{    
+    private:
+        // key = source
+        // tuple: destination, delay, flight num
+        unordered_map<string, vector<tuple<string, int, int>>> airport;
 
+    public:
+        graph();
+        //
+        void addEdge(tuple<string, string, string> passedData){
+            
+        };
 
 };
